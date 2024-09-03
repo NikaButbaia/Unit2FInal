4238566afd7cbfc741056e3e6f884a2210acb0cb
NikaButbaia <nikazerff@gmail.com>
2024-09-03 19:43:41 +0400

    third commit

--- test5.js
+++ test5.js
@@ -5,7 +5,9 @@ describe("Testing framework Final with Accuweather", () => {
     browser.url("https://www.accuweather.com/");
     await $(`//div[contains(@class, 'policy-accept')]`).click();
     await $(`//input[@name='query']`).setValue("New York");
-    browser.pause(7000);
-    await $(`//div[@class="results-container"]/div[1]`).click();
+    const resultContainer = await $(`//div[@class="results-container"]/div[1]`);
+    resultContainer.waitForDisplayed({ timeout: 10000 });
+    resultContainer.click();
+    // await browser.debug();
   });
 });
